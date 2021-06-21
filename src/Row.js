be5238f7259893081a8ce302fda583fea402dff3
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-21 17:07:50 +0200

    Rows with styling

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'; //(onze axios-file)
+import './Row.css';
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row( {title, fetchUrl}) { //{props}
     //state toevoegen 42:01
@@ -21,8 +23,19 @@ function Row( {title, fetchUrl}) { //{props}
     console.log(movies);
 
     return (
-        <div>
+        <div className="row">
             <h2>{title }</h2>
+
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
 
             {/* container -> posters */}
         </div>
