b6a47017b2d7f25d9a7c6a09b3a73c3984a0a35b
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-06-27 12:41:23 +0200

    Added trailer popups with YouTube

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'; //(onze axios-file)
 import './Row.css';
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row( {title, fetchUrl, isLargeRow}) { //{props}
     //state toevoegen 42:01
     const [movies, setMovies] = useState([]);
+
+    //State aanmaken die de trailerURLs ophaalt (2:49:02)
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     //Nu een snippet of code invoeren die runt op een specific condition/variable -->useEffect
     //Goed voorbeeld dit voor verder: rond 55:00 - 58:00
@@ -20,6 +25,28 @@ function Row( {title, fetchUrl, isLargeRow}) { //{props}
         //if [], run once when row loads, and dont run again.
     }, [fetchUrl]);
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        }
+    }
+
+    //Function handleClick() maken
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "") //Dit is dus een promise
+                .then(url => {
+                    const urlParams = new URLSearchParams( new URL(url).search);
+                    setTrailerUrl(urlParams.get('v'));
+                })
+                .catch(error => console.log(error))
+        }
+    }
+
     console.log(movies);
 
     return (
@@ -30,12 +57,14 @@ function Row( {title, fetchUrl, isLargeRow}) { //{props}
                 {movies.map(movie => (
                     <img
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
                         alt={movie.name}
                     />
                 ))}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
 
             {/* container -> posters */}
         </div>
